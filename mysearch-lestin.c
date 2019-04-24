28f883f8aea1a660124c157c40405472d7b26736
Arnold Lestin <amlestin@gmail.com>
2019-04-23 20:23:50 -0400

    mysearch complete

--- mysearch-lestin.c
+++ mysearch-lestin.c
@@ -1,14 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+
+void print_until_end(char *path_to_dir, int indent);
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Error: Not enough arguments.\n");
-        printf("Usage: ./mysearch [filename]\n");
-        return 1;
+    char *starting_directory;
+    char default_directory[2] = ".";
+    if (argv[1] == NULL) {
+        starting_directory = &default_directory[0];
+    } else {
+        starting_directory = argv[1];
     }
-    char *file_name_to_seek = argv[1];
 
+    printf("%s\n", starting_directory);
 
+    print_until_end(starting_directory, 0);
 
     return 0;
+}
+
+void print_until_end(char *path_to_dir, int indent) {
+    char slash[2] = "/";
+    DIR *cur_dir = opendir(path_to_dir);
+    struct dirent *de;
+
+    while ( (de = readdir(cur_dir)) != NULL) {
+        if (de->d_name[0] == '.')
+            continue;
+
+        if (de->d_type == DT_DIR) {
+            for (int i = 0; i < indent; i++) {
+                printf(" ");
+            }
+            printf("├");
+            printf("──%s\n", de->d_name);
+
+            char *new_path = calloc(strlen(path_to_dir) + strlen(&slash[0]) + strlen(de->d_name) + 1, sizeof(char));
+            strcat(new_path, path_to_dir);
+            strcat(new_path, &slash[0]);
+            strcat(new_path, de->d_name);
+
+            int new_indent = indent + 1;
+            print_until_end(new_path, new_indent);
+
+            free(new_path);
+        } else {
+            for (int i = 0; i < indent; i++) {
+                printf(" ");
+            }
+
+            printf("├");
+            printf("──%s\n", de->d_name);
+        }
+    }
 }
