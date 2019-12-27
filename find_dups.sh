a17bfc24f3bea37788cb48e85728fb074edce2a2
Arnold Lestin <amlestin@gmail.com>
2019-12-27 01:00:07 -0500

    Update find_dups.sh

--- find_dups.sh
+++ find_dups.sh
@@ -2,6 +2,7 @@
 
 
 # prints redundant files in directory
+# this solution respects the UNIX philosophy and makes use of minimal new code
 
 DIR=${1?Error: no directory chosen}
 ./lsrecr $DIR | sort | uniq -d -c
